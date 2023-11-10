05c60efd2ddd9c3fd1362448772c7539bb86e35b
sihyunjojo <si4018@naver.com>
2023-11-10 17:18:45 +0900

    feat: 총 주문 금액에서 해당 금액만큼 할인 하는 기능 구현

---
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -0,0 +1,14 @@
+package christmas.controller;
+
+import christmas.domain.DDayDiscount;
+
+public class EventPlannerController {
+    public static void planEvent() {
+        int allOrderPrice = 0;
+        int day = 1;
+        DDayDiscount dDayDiscount = DDayDiscount.createDDayDiscount(day);
+        int currentDiscountPrice = dDayDiscount.getCurrentDiscountPrice();
+
+        int discountedPrices= allOrderPrice - currentDiscountPrice;
+    }
+}
