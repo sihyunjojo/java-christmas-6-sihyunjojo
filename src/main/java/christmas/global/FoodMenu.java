0718030e3a460b99d9a6995837375675760c1049
sihyunjojo <si4018@naver.com>
2023-11-11 11:25:18 +0900

    refactor: Enum명 영어로 교체

--- src/main/java/christmas/global/FoodMenu.java
+++ src/main/java/christmas/global/FoodMenu.java
@@ -1,19 +1,21 @@
+package christmas.global;
+
 public enum FoodMenu {
-    양송이수프("Appetizer", "양송이수프", 6000),
-    타파스("Appetizer", "타파스", 5500),
-    시저샐러드("Appetizer", "시저샐러드", 8000),
+    MUSHROOM_SOUP("Appetizer", "양송이수프", 6000),
+    TAPAS("Appetizer", "타파스", 5500),
+    CAESAR_SALAD("Appetizer", "시저샐러드", 8000),
 
-    티본스테이크("Main", "티본스테이크", 55000),
-    바비큐립("Main", "바비큐립", 54000),
-    해산물파스타("Main", "해산물파스타", 35000),
-    크리스마스파스타("Main", "크리스마스파스타", 25000),
+    T_BONE_STEAK("Main", "티본스테이크", 55000),
+    BBQ_RIBS("Main", "바비큐립", 54000),
+    SEAFOOD_PASTA("Main", "해산물파스타", 35000),
+    CHRISTMAS_PASTA("Main", "크리스마스파스타", 25000),
 
-    초코케이크("Dessert", "초코케이크", 15000),
-    아이스크림("Dessert", "아이스크림", 5000),
+    CHOCOLATE_CAKE("Dessert", "초코케이크", 15000),
+    ICE_CREAM("Dessert", "아이스크림", 5000),
 
-    제로콜라("Beverage", "제로콜라", 3000),
-    레드와인("Beverage", "레드와인", 60000),
-    샴페인("Beverage", "샴페인", 25000);
+    ZERO_COLA("Beverage", "제로콜라", 3000),
+    RED_WINE("Beverage", "레드와인", 60000),
+    CHAMPAGNE("Beverage", "샴페인", 25000);
 
     private final String category;
     private final String name;
