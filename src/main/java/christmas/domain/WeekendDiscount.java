ac5c09d19295a00bb31273203ed09f7fe423d808
sihyunjojo <si4018@naver.com>
2023-11-11 11:26:03 +0900

    feat: 주말 할인 객체에 주말에는 메인 메뉴를 1개 당 2023원 할인해주는 기능 추가

---
+++ src/main/java/christmas/domain/WeekendDiscount.java
@@ -0,0 +1,41 @@
+package christmas.domain;
+
+import static christmas.global.DisCountPriceConstants.WEEKEND_DISCOUNT_PRICE;
+
+import christmas.global.FoodMenu;
+import java.util.Map;
+
+public class WeekendDiscount {
+    private final int discountPrice = WEEKEND_DISCOUNT_PRICE;
+
+    public WeekendDiscount() {
+    }
+
+    public static WeekendDiscount createWeekendDiscount() {
+        return new WeekendDiscount();
+    }
+
+    public int discountAllOrderPrice(Order order) {
+        Map<FoodMenu, Integer> foodMenus = order.foodMenus();
+        int disCountedDessertPrice = calculateDiscountedDessertPrice(foodMenus);
+        int otherFoodMenuPrice = calculateDiscountedOtherFoodMenuPrice(foodMenus);
+
+        return disCountedDessertPrice + otherFoodMenuPrice;
+    }
+
+    private static int calculateDiscountedOtherFoodMenuPrice(Map<FoodMenu, Integer> foodMenus) {
+        return foodMenus.entrySet()
+                .stream()
+                .filter(foodMenu -> !foodMenu.getKey().getCategory().equals("Main"))
+                .mapToInt(otherFoodMenu -> otherFoodMenu.getValue() * otherFoodMenu.getKey().getPrice())
+                .sum();
+    }
+
+    private int calculateDiscountedDessertPrice(Map<FoodMenu, Integer> foodMenus) {
+        return foodMenus.entrySet()
+                .stream()
+                .filter(foodMenu -> foodMenu.getKey().getCategory().equals("Main"))
+                .mapToInt(dessertMenu -> dessertMenu.getValue() * (dessertMenu.getKey().getPrice() - discountPrice))
+                .sum();
+    }
+}
