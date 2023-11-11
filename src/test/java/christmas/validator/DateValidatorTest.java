3262b2fc77fd520d76425697b566c8763b17d6cf
sihyunjojo <si4018@naver.com>
2023-11-11 16:57:28 +0900

    test: 날짜검증 클래스의 메서드에 대해서 테스트 구현

--- src/test/java/christmas/validator/DateValidatorTest.java
+++ src/test/java/christmas/validator/DateValidatorTest.java
@@ -16,7 +16,6 @@ class DateValidatorTest {
     @CsvSource(value = {"2022:10:10", "2023:12:26", "2024:01:01"}, delimiter = ':')
     @ParameterizedTest
     void testInvalidateDDayDisCountPeriod(int year, int month, int day) {
-
         // given
         LocalDate inputDate = LocalDate.of(year, month, day);
 
@@ -33,6 +32,49 @@ class DateValidatorTest {
         // then
         assertDoesNotThrow(() -> DateValidator.validateDDayDisCountPeriod(inputDate));
     }
+
+    @DisplayName("12월 할인기간 이외의 기간은 잘못된 날짜 예외 에러를 발생시켜준다.")
+    @CsvSource(value = {"2022:10:10", "2023:11:30", "2024:01:01"}, delimiter = ':')
+    @ParameterizedTest
+    void testInvalidateDisCountPeriod(int year, int month, int day) {
+        // given
+        LocalDate inputDate = LocalDate.of(year, month, day);
+
+        // then
+        assertThrows(InvalidDateException.class, () -> DateValidator.validateDisCountPeriod(inputDate));
+    }
+    @DisplayName("12월 할인기간 이내의 기간이 들어오면, 예외가 발생하지 않는다.")
+    @ValueSource(ints = {1, 10, 25, 31})
+    @ParameterizedTest
+    void testValidateDisCountPeriod(int day) {
+        // given
+        LocalDate inputDate = LocalDate.of(2023, 12, day);
+
+        // then
+        assertDoesNotThrow(() -> DateValidator.validateDisCountPeriod(inputDate));
+    }
+
+    @DisplayName("달력에 별이 있는 날짜 이외가 들어오면, 잘못된 날짜 예외 에러를 발생시켜준다.")
+    @CsvSource(value = {"2022:10:10", "2023:11:30", "2024:01:01"}, delimiter = ':')
+    @ParameterizedTest
+    void testInvalidateSpecialDisCountPeriod(int year, int month, int day) {
+        // given
+        LocalDate inputDate = LocalDate.of(year, month, day);
+
+        // then
+        assertThrows(InvalidDateException.class, () -> DateValidator.validateSpecialDisCountPeriod(inputDate));
+    }
+    @DisplayName("달력에 별이 있는 날짜가 들어오면, 예외가 발생하지 않는다.")
+    @ValueSource(ints = {3, 10, 17, 24, 25, 31})
+    @ParameterizedTest
+    void testValidateSpecialDisCountPeriod(int day) {
+        // given
+        LocalDate inputDate = LocalDate.of(2023, 12, day);
+
+        // then
+        assertDoesNotThrow(() -> DateValidator.validateSpecialDisCountPeriod(inputDate));
+    }
+
 
 
 }
