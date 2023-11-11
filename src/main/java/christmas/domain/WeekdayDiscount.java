505c2ba3d341d4ec9956516aa4ef7d6adcda7e9a
sihyunjojo <si4018@naver.com>
2023-11-11 11:25:46 +0900

    feat: 평일 할인 객체에 평일에는 디저트 메뉴를 1개 당 2023 할인해주는 기능 추가

---
+++ src/main/java/christmas/domain/WeekdayDiscount.java
@@ -0,0 +1,43 @@
+package christmas.domain;
+
+import static christmas.global.DisCountPriceConstants.WEEKDAY_DISCOUNT_PRICE;
+
+import christmas.global.FoodMenu;
+import java.util.Map;
+
+public class WeekdayDiscount {
+
+    private final int discountPrice = WEEKDAY_DISCOUNT_PRICE;
+
+    public WeekdayDiscount() {
+    }
+
+    public static WeekdayDiscount createWeekdayDiscount() {
+        return new WeekdayDiscount();
+    }
+
+    public int discountAllOrderPrice(Order order) {
+        Map<FoodMenu, Integer> foodMenus = order.foodMenus();
+        int disCountedDessertPrice = calculateDiscountedDessertPrice(foodMenus);
+        int otherFoodMenuPrice = calculateDiscountedOtherFoodMenuPrice(foodMenus);
+
+        return disCountedDessertPrice + otherFoodMenuPrice;
+    }
+
+    private static int calculateDiscountedOtherFoodMenuPrice(Map<FoodMenu, Integer> foodMenus) {
+        return foodMenus.entrySet()
+                .stream()
+                .filter(foodMenu -> !foodMenu.getKey().getCategory().equals("Dessert"))
+                .mapToInt(otherFoodMenu -> otherFoodMenu.getValue() * otherFoodMenu.getKey().getPrice())
+                .sum();
+    }
+
+    private int calculateDiscountedDessertPrice(Map<FoodMenu, Integer> foodMenus) {
+        return foodMenus.entrySet()
+                .stream()
+                .filter(foodMenu -> foodMenu.getKey().getCategory().equals("Dessert"))
+                .mapToInt(dessertMenu -> dessertMenu.getValue() * (dessertMenu.getKey().getPrice() - discountPrice))
+                .sum();
+    }
+
+}
--- src/main/java/christmas/global/DisCountPriceConstants.java
+++ src/main/java/christmas/global/DisCountPriceConstants.java
@@ -3,4 +3,7 @@ package christmas.global;
 public class DisCountPriceConstants {
     public static int DISCOUNT_PRICE_INCREASE_PER_DAY = 100;
     public static int INITIAL_D_DAY_DISCOUNT_PRICE = 1000;
+    public static int WEEKDAY_DISCOUNT_PRICE = 2023;
+    public static int WEEKEND_DISCOUNT_PRICE = 2023;
+    public static int SPECIAL_DISCOUNT_PRICE = 1000;
 }
