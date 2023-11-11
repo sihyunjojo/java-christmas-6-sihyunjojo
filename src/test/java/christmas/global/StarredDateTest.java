233b0951c8e1ec02ce17e7df0ef322032a5d15a5
sihyunjojo <si4018@naver.com>
2023-11-11 17:04:42 +0900

    test: StarredDateTest의 달력에 별표시 된 날짜인지 알려주는 메서드에 대한 테스트 구현

---
+++ src/test/java/christmas/global/StarredDateTest.java
@@ -0,0 +1,29 @@
+package christmas.global;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class StarredDateTest {
+    @ParameterizedTest
+    @ValueSource(ints = {3, 10, 25, 31})
+    @DisplayName("달력에 별표시 된 날짜가 들어오면 true를 반환해준다.")
+    void testIsStarredDateInValidDate(int day) {
+        LocalDate date = LocalDate.of(2023, 12, day);
+
+        assertTrue(StarredDate.isStarredDate(date));
+    }
+    @ParameterizedTest
+    @CsvSource(value = {"2023:12:1","2023:12:30","2024:01:01"}, delimiter = ':')
+    @DisplayName("달력에 별표시 안된 날짜가 들어오면 False를 반환해준다.")
+    void testIsStarredDateInInvalidDate(int year,int month, int day) {
+        LocalDate date = LocalDate.of(year, month, day);
+
+        assertFalse(StarredDate.isStarredDate(date));
+    }
+
+}
