5fdfc00a793d738cda1bd2b373a3ec8aca58e8c4
sihyunjojo <si4018@naver.com>
2023-11-11 15:29:44 +0900

    feat: 주문 객체에 주문전체가격 계산하는 기능 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -4,6 +4,7 @@ import christmas.global.FoodMenu;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public record Order(Map<FoodMenu, Integer> foodMenus) {
     public Order(Map<FoodMenu, Integer> foodMenus) {
@@ -12,6 +13,17 @@ public record Order(Map<FoodMenu, Integer> foodMenus) {
 
     public static Order createOrder(Map<FoodMenu, Integer> foodMenus) {
         return new Order(foodMenus);
+    }
+
+    public int getOrderAllPrice(){
+        return foodMenus.entrySet()
+                .stream()
+                .mapToInt(Order::calculateFoodMenuAllPrice)
+                .sum();
+    }
+
+    private static int calculateFoodMenuAllPrice(Entry<FoodMenu, Integer> foodMenu) {
+        return foodMenu.getKey().getPrice() * foodMenu.getValue();
     }
 
     @Override
