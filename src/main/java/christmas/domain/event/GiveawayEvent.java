04b0d9d13c70458e010658222e7a707b6ae0b870
sihyunjojo <si4018@naver.com>
2023-11-11 15:31:39 +0900

    feat: 증정 상품이벤트에 기간 검증 기능 추가

--- src/main/java/christmas/domain/event/GiveawayEvent.java
+++ src/main/java/christmas/domain/event/GiveawayEvent.java
@@ -5,6 +5,8 @@ import static christmas.global.FoodMenu.*;
 
 import christmas.domain.Order;
 import christmas.global.FoodMenu;
+import christmas.validator.DateValidator;
+import java.time.LocalDate;
 
 public class GiveAwayEvent {
     // GiveawayProduct라는 객체 만들어서 메뉴와 개수 관리하면 좋을거 같다.
@@ -13,7 +15,8 @@ public class GiveAwayEvent {
     public GiveAwayEvent() {
     }
 
-    public static GiveAwayEvent createGiveawayEvent(Order order) {
+    public static GiveAwayEvent createGiveawayEvent(Order order, LocalDate date) {
+        DateValidator.validateDisCountPeriod(date);
         if (GIVE_AWAY_PRODUCT_PRICE_CONDITION <= order.getOrderAllPrice()) {
             return new GiveAwayEvent();
         }
