3ebc784c5f0a26c0ee99adde5d55bf9aba215725
sihyunjojo <si4018@naver.com>
2023-11-13 15:30:10 +0900

    refactor: 객체 생성시 검증 내부 검증에서 외부 검증으로 변경에 따른 WeekendDiscount객체 변경

--- src/main/java/christmas/domain/event/discount/WeekendDiscount.java
+++ src/main/java/christmas/domain/event/discount/WeekendDiscount.java
@@ -1,47 +1,49 @@
 package christmas.domain.event.discount;
 
+import static christmas.global.BenefitDetail.WEEKEND_DISCOUNT;
 import static christmas.global.constants.DisCountPriceConstants.WEEKEND_DISCOUNT_PRICE;
 
+import christmas.domain.Benefit;
 import christmas.domain.Order;
 import christmas.global.FoodMenu;
-import christmas.validator.DateValidator;
-import java.time.LocalDate;
 import java.util.Map;
 import java.util.Objects;
 
-public class WeekendDiscount {
-    private final int discountPrice = WEEKEND_DISCOUNT_PRICE;
+public class WeekendDiscount extends Benefit {
+    private final int allDisCountPrice;
 
-    public WeekendDiscount() {
+    public WeekendDiscount(int allDisCountPrice) {
+        this.allDisCountPrice = allDisCountPrice;
     }
 
-    public static WeekendDiscount createWeekendDiscount(LocalDate date) {
-        DateValidator.validateDisCountPeriod(date);
-        return new WeekendDiscount();
+    public static WeekendDiscount createWeekendDiscount(Order order) {
+        int allDisCountPrice = calculateDiscountedPrice(order);
+        return new WeekendDiscount(allDisCountPrice);
     }
 
-    public int discountAllOrderPrice(Order order) {
+    public int discountOrderPrice(Order order) {
+        int orderPrice = order.getOrderPrice();
+        return orderPrice - allDisCountPrice;
+    }
+
+    private static int calculateDiscountedPrice(Order order) {
         Map<FoodMenu, Integer> foodMenus = order.foodMenus();
-        int disCountedDessertPrice = calculateDiscountedDessertPrice(foodMenus);
-        int otherFoodMenuPrice = calculateOtherFoodMenuPrice(foodMenus);
 
-        return disCountedDessertPrice + otherFoodMenuPrice;
-    }
-
-    public int calculateOtherFoodMenuPrice(Map<FoodMenu, Integer> foodMenus) {
-        return foodMenus.entrySet()
-                .stream()
-                .filter(foodMenu -> !foodMenu.getKey().getCategory().equals("Main"))
-                .mapToInt(otherFoodMenu -> otherFoodMenu.getValue() * otherFoodMenu.getKey().getPrice())
-                .sum();
-    }
-
-    public int calculateDiscountedDessertPrice(Map<FoodMenu, Integer> foodMenus) {
         return foodMenus.entrySet()
                 .stream()
                 .filter(foodMenu -> foodMenu.getKey().getCategory().equals("Main"))
-                .mapToInt(dessertMenu -> dessertMenu.getValue() * (dessertMenu.getKey().getPrice() - discountPrice))
+                .mapToInt(mainMenu -> (mainMenu.getValue() *  WEEKEND_DISCOUNT_PRICE))
                 .sum();
+    }
+
+    @Override
+    public String getDescription() {
+        return WEEKEND_DISCOUNT.getMessage();
+    }
+
+    @Override
+    public int getBenefitPrice() {
+        return allDisCountPrice;
     }
 
     @Override
@@ -53,11 +55,11 @@ public class WeekendDiscount {
             return false;
         }
         WeekendDiscount that = (WeekendDiscount) o;
-        return discountPrice == that.discountPrice;
+        return allDisCountPrice == that.allDisCountPrice;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(discountPrice);
+        return Objects.hash(allDisCountPrice);
     }
 }
