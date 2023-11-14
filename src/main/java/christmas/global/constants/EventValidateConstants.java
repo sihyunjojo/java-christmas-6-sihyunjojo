72f212587fbcddace60a57466f1bf657be0206bb
sihyunjojo <si4018@naver.com>
2023-11-14 23:31:19 +0900

    feat: 새 상수 추가

--- src/main/java/christmas/global/constants/EventValidateConstants.java
+++ src/main/java/christmas/global/constants/EventValidateConstants.java
@@ -5,5 +5,8 @@ public class EventValidateConstants {
     public static int EVENT_PRICE_CONDITION = 10000;
     public static String RESTRICTED_CATEGORY = "Beverage";
     public static int MAX_FOOD_MENU = 20;
+    public static int MIN_FOOD_MENU = 1;
+    public static int MAX_DAY = 31;
+    public static int MIN_DAY = 1;
 
 }
