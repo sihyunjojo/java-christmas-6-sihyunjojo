4ef1bb7e17142e5d62bcfff28fc79b84d3770886
sihyunjojo <si4018@naver.com>
2023-11-15 00:12:18 +0900

    test: EventValidator 메서드 추가에 따른 테스트 코드 추가

--- src/test/java/christmas/validator/EventValidatorTest.java
+++ src/test/java/christmas/validator/EventValidatorTest.java
@@ -2,9 +2,11 @@ package christmas.validator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import christmas.exception.InvalidDateException;
 import christmas.exception.InvalidMenuException;
 import christmas.exception.InvalidPriceException;
 import christmas.global.FoodMenu;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,17 +23,39 @@ class EventValidatorTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1000, 4999})
-    @DisplayName("이벤트의 가격 조건에 맞지 않으면, 예외를 발생시켜준다.")
+    @DisplayName("이벤트의 가격 조건에 맞지 않으면, False를 반환해준다.")
     void testInValidateOrderPrice(int orderPrice) {
-        Assertions.assertThrows(InvalidPriceException.class, () -> EventValidator.validateOrderPrice(orderPrice));
+        Assertions.assertFalse(EventValidator.validateOrderPrice(orderPrice));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {10000, 100000, 200000})
-    @DisplayName("이벤트의 가격 조건에 맞지 않으면, 예외가 나타나지 않는다.")
+    @DisplayName("이벤트의 가격 조건에 맞으면, True를 반환해준다.")
     void testValidateOrderPrice(int orderPrice) {
-        Assertions.assertDoesNotThrow( () -> EventValidator.validateOrderPrice(orderPrice));
+        Assertions.assertTrue(EventValidator.validateOrderPrice(orderPrice));
     }
+
+    @DisplayName("12월 할인기간 이외의 기간은 잘못된 날짜 예외 에러를 발생시켜준다.")
+    @CsvSource(value = {"2022:10:10", "2023:11:30", "2024:01:01"}, delimiter = ':')
+    @ParameterizedTest
+    void testInvalidateDisCountPeriod(int year, int month, int day) {
+        // given
+        LocalDate inputDate = LocalDate.of(year, month, day);
+
+        // then
+        assertThrows(InvalidDateException.class, () -> EventValidator.validateDisCountPeriod(inputDate));
+    }
+    @DisplayName("12월 할인기간 이내의 기간이 들어오면, 예외가 발생하지 않는다.")
+    @ValueSource(ints = {1, 10, 25, 31})
+    @ParameterizedTest
+    void testValidateDisCountPeriod(int day) {
+        // given
+        LocalDate inputDate = LocalDate.of(2023, 12, day);
+
+        // then
+        assertDoesNotThrow(() -> EventValidator.validateDisCountPeriod(inputDate));
+    }
+
 
     @Test
     @DisplayName("음료만 시킬 경우, 예외를 발생시켜준다.")
