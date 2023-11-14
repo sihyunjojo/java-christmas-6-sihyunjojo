402316c501d4b158621038bca4ca74b22673830e
sihyunjojo <si4018@naver.com>
2023-11-14 23:23:41 +0900

    feat: OutputView 클래스 생성

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,27 @@
+package christmas.view;
+
+import christmas.message.MessageProvider;
+import java.util.List;
+
+public class OutputView {
+    private OutputView() {
+    }
+
+    public static void printMessage(MessageProvider message) {
+        System.out.println(message.getMessage());
+    }
+    public static <T> void print(T message){
+        System.out.println(message);
+    }
+    public static <T> void printWithBlankLine(T message) {
+        System.out.println(message);
+        printMessageBlankLine();
+    }
+    public static <T> void printMessagesWithBlankLine(List<T> messages) {
+        messages.forEach(System.out::println);
+        printMessageBlankLine();
+    }
+    public static void printMessageBlankLine() {
+        System.out.println();
+    }
+}
