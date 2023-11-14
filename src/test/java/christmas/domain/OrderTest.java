90fd0086fc0d9467d5f2065ed8163dd6bc6d2e7d
sihyunjojo <si4018@naver.com>
2023-11-15 00:04:44 +0900

    test: Order객체에 대한 테스트

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,4 +1,36 @@
+package christmas.domain;
+
+import static christmas.global.FoodMenu.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.global.FoodMenu;
+import java.util.HashMap;
+import java.util.Map;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class OrderTest {
-  
+
+    @Test
+    @DisplayName("Order 객체 생성 및 속성 확인")
+    void testCreateOrder() {
+        Map<FoodMenu, Integer> foodMenus = new HashMap<>();
+        foodMenus.put(T_BONE_STEAK, 1);
+        foodMenus.put(BBQ_RIBS, 1);
+        foodMenus.put(CHOCOLATE_CAKE, 2);
+        foodMenus.put(ZERO_COLA, 1);
+
+        Order order = Order.createOrder(foodMenus);
+
+        Assertions.assertEquals(order, new Order(foodMenus));
+        Assertions.assertEquals(order.getOrderPrice(),
+                T_BONE_STEAK.getPrice() + BBQ_RIBS.getPrice() +
+                        (CHOCOLATE_CAKE.getPrice() * 2) + ZERO_COLA.getPrice());
+        Assertions.assertEquals(order.getFoodMenuCount(),
+                1 + 1 + 2 + 1);
+        Assertions.assertEquals(order.getFoodMenus(),
+                foodMenus.keySet().stream().toList());
+    }
 }
