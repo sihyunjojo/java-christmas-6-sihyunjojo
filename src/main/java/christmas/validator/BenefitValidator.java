4a081ad5ab29b3a37245f2894c5a7979b9496111
sihyunjojo <si4018@naver.com>
2023-11-14 14:56:02 +0900

    feat: 혜택들을 검증해주는 혜택 검증 클래스 생성

---
+++ src/main/java/christmas/validator/BenefitValidator.java
@@ -0,0 +1,33 @@
+package christmas.validator;
+
+import static christmas.global.constants.DiscountDateConstants.*;
+
+import christmas.global.StarredDate;
+import christmas.global.constants.EventValidateConstants;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class BenefitValidator {
+    public static boolean isDDayDiscountValid(LocalDate date){
+        return !date.isBefore(D_DAY_DISCOUNT_START_DAY) && !date.isAfter(D_DAY_DISCOUNT_END_DAY);
+    }
+    public static boolean isSpecialDiscountValid(LocalDate date){
+        return StarredDate.isStarredDate(date);
+    }
+    public static boolean isWeekdayDiscountValid(LocalDate date){
+        return !isWeekend(date);
+    }
+    public static boolean isWeekendDiscountValid(LocalDate date){
+        return isWeekend(date);
+    }
+    public static boolean isGiveAwayEventValid(int orderPrice){
+        return orderPrice >= EventValidateConstants.GIVE_AWAY_EVENT_PRICE_CONDITION;
+    }
+
+
+    public static boolean isWeekend(LocalDate date) {
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY;
+    }
+
+}
