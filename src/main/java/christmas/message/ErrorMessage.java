a674014f8aa3e9abf75f912f5909609e5e9d0350
sihyunjojo <si4018@naver.com>
2023-11-15 02:07:41 +0900

    refactor: Enum 이름 변경

--- src/main/java/christmas/message/ErrorMessage.java
+++ src/main/java/christmas/message/ErrorMessage.java
@@ -1,9 +1,9 @@
 package christmas.message;
 
 public enum ErrorMessage implements MessageProvider{
-    INVALID_DATE("[ERROR] 잘못된 날짜입니다."),
+    INVALID_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     INVALID_PRICE("[ERROR] 잘못된 가격입니다."),
-    INVALID_MENU("[ERROR] 잘못된 메뉴입니다."),
+    INVALID_ORDER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
     INVALID_INPUT("[ERROR] 잘못된 입력입니다.");
     private final String message;
 
