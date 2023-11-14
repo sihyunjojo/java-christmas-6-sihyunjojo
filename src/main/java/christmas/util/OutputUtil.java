121815ba89c6684c7d744b136b98555636cda127
sihyunjojo <si4018@naver.com>
2023-11-14 23:25:10 +0900

    feat: OutputUtil 클래스 생성

---
+++ src/main/java/christmas/util/OutputUtil.java
@@ -0,0 +1,11 @@
+package christmas.util;
+
+import christmas.domain.Benefit;
+
+public class OutputUtil {
+    public static String priceToString(int OrderPrice) {
+        return String.format("%,d원", OrderPrice);
+    }
+
+
+}
