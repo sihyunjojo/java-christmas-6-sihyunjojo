d4df327d0462baa543302b48e9d8bdfecb4605df
sihyunjojo <si4018@naver.com>
2023-11-14 23:24:55 +0900

    feat: InputHandler클래스 생성

---
+++ src/main/java/christmas/handler/InputHandler.java
@@ -0,0 +1,41 @@
+package christmas.handler;
+
+import static christmas.message.SystemMessage.*;
+import static christmas.validator.InputFoodMenusValidator.*;
+
+import christmas.global.FoodMenu;
+import christmas.util.InputUtil;
+import christmas.validator.InputDateValidator;
+import christmas.validator.InputValidator;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.time.LocalDate;
+import java.util.Map;
+
+public class InputHandler {
+    public static LocalDate setDate(){
+        OutputView.printMessage(INPUT_DAY);
+
+        String inputDay = InputView.read();
+        InputValidator.validateInputStringParseInteger(inputDay);
+
+        int day = InputUtil.StringToInt(inputDay);
+        InputDateValidator.validateInputDayPeriod(day);
+
+        return LocalDate.of(2023, 12, day);
+    }
+
+    public static Map<FoodMenu, Integer> setFoodMenus(){
+        OutputView.printMessage(INPUT_FOOD_MENUS);
+
+        String inputFoodMenus = InputView.read();
+        String[] splitFoodMenus = InputUtil.splitStringByComma(inputFoodMenus);
+        validateInputStringFormat(splitFoodMenus);
+
+        Map<FoodMenu, Integer> foodMenus = InputUtil.StringsToFoodMenus(splitFoodMenus);
+        validateInputFoodMenuCount(foodMenus);
+        validateInputFoodMenuIncludingDuplicate(foodMenus);
+
+        return foodMenus;
+    }
+}
