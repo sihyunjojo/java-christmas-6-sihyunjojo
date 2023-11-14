9462d664c3a4d8b69f627646d8a9a4951a44ed79
sihyunjojo <si4018@naver.com>
2023-11-14 23:29:08 +0900

    refactor: Enum추가 및 인터페이스 상속 추가

--- src/main/java/christmas/message/ErrorMessage.java
+++ src/main/java/christmas/message/ErrorMessage.java
@@ -1,8 +1,10 @@
 package christmas.message;
 
-public enum ErrorMessage {
-    INVALID_DATE("[ERROR] 잘못된 날짜입니다.");
-
+public enum ErrorMessage implements MessageProvider{
+    INVALID_DATE("[ERROR] 잘못된 날짜입니다."),
+    INVALID_PRICE("[ERROR] 잘못된 가격입니다."),
+    INVALID_MENU("[ERROR] 잘못된 메뉴입니다."),
+    INVALID_INPUT("[ERROR] 잘못된 입력입니다.");
     private final String message;
 
     ErrorMessage(String message) {
