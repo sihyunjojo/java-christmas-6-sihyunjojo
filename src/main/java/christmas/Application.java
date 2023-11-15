060a5ca9748515e2d148955773f0c390a713b694
sihyunjojo <si4018@naver.com>
2023-11-15 20:17:00 +0900

    refactor: 메인 실행 다시 생성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -5,6 +5,6 @@ import christmas.controller.EventPlannerController;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        EventPlannerController.planEvent();
+        EventPlannerController.planEvent();
     }
 }
