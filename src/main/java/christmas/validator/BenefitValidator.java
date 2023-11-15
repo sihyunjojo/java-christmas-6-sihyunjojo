1c4990bb7051be1ec22043fa52bba220d9d654ad
sihyunjojo <si4018@naver.com>
2023-11-15 14:21:00 +0900

    feat: 디데이 할인 객체를 제외한 다른 혜택 생성에 날짜 검증 코드 추가

--- src/main/java/christmas/validator/BenefitValidator.java
+++ src/main/java/christmas/validator/BenefitValidator.java
@@ -6,24 +6,28 @@ import christmas.global.StarredDate;
 import christmas.global.constants.EventValidateConstants;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Date;
 
 public class BenefitValidator {
     public static boolean isDDayDiscountValid(LocalDate date){
         return !date.isBefore(D_DAY_DISCOUNT_START_DAY) && !date.isAfter(D_DAY_DISCOUNT_END_DAY);
     }
     public static boolean isSpecialDiscountValid(LocalDate date){
-        return StarredDate.isStarredDate(date);
+        return StarredDate.isStarredDate(date) && isDiscountValid(date);
     }
     public static boolean isWeekdayDiscountValid(LocalDate date){
-        return !isWeekend(date);
+        return !isWeekend(date)  && isDiscountValid(date);
     }
     public static boolean isWeekendDiscountValid(LocalDate date){
-        return isWeekend(date);
+        return isWeekend(date)  && isDiscountValid(date);
     }
-    public static boolean isGiveAwayEventValid(int orderPrice){
-        return orderPrice >= EventValidateConstants.GIVE_AWAY_EVENT_PRICE_CONDITION;
+    public static boolean isGiveAwayEventValid(LocalDate date, int orderPrice){
+        return orderPrice >= EventValidateConstants.GIVE_AWAY_EVENT_PRICE_CONDITION  && isDiscountValid(date);
     }
 
+    public static boolean isDiscountValid(LocalDate date) {
+        return !date.isBefore(DECEMBER_DISCOUNT_START_DAY) && !date.isAfter(DECEMBER_DISCOUNT_END_DAY);
+    }
     public static boolean isWeekend(LocalDate date) {
         DayOfWeek dayOfWeek = date.getDayOfWeek();
         return dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.FRIDAY;
