ac95dd050595f1a4a99abfd078fac7ad3c9a697b
sihyunjojo <si4018@naver.com>
2023-11-15 16:28:58 +0900

    refactor: 사용하지 않는 enum 삭제

--- src/main/java/christmas/message/ErrorMessage.java
+++ src/main/java/christmas/message/ErrorMessage.java
@@ -2,7 +2,6 @@ package christmas.message;
 
 public enum ErrorMessage implements MessageProvider{
     INVALID_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
-    INVALID_PRICE("[ERROR] 잘못된 가격입니다. 다시 입력해 주세요"),
     INVALID_ORDER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
     INVALID_INPUT("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요");
     private final String message;
