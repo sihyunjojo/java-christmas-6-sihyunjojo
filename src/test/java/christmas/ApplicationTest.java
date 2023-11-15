0dd4724061f1607fd204c29f68230668d5d32dd6
sihyunjojo <si4018@naver.com>
2023-11-15 19:47:06 +0900

    test:  혜택_내역_없음_출력 테스트

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -9,29 +9,29 @@ import org.junit.jupiter.api.Test;
 class ApplicationTest extends NsTest {
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
-    @Test
-    void 모든_타이틀_출력() {
-        assertSimpleTest(() -> {
-            run("3", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
-            assertThat(output()).contains(
-                "<주문 메뉴>",
-                "<할인 전 총주문 금액>",
-                "<증정 메뉴>",
-                "<혜택 내역>",
-                "<총혜택 금액>",
-                "<할인 후 예상 결제 금액>",
-                "<12월 이벤트 배지>"
-            );
-        });
-    }
-
 //    @Test
-//    void 혜택_내역_없음_출력() {
+//    void 모든_타이틀_출력() {
 //        assertSimpleTest(() -> {
-//            run("26", "타파스-1,제로콜라-1");
-//            assertThat(output()).contains("<혜택 내역>" + LINE_SEPARATOR + "없음");
+//            run("3", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
+//            assertThat(output()).contains(
+//                "<주문 메뉴>",
+//                "<할인 전 총주문 금액>",
+//                "<증정 메뉴>",
+//                "<혜택 내역>",
+//                "<총혜택 금액>",
+//                "<할인 후 예상 결제 금액>",
+//                "<12월 이벤트 배지>"
+//            );
 //        });
 //    }
+
+    @Test
+    void 혜택_내역_없음_출력() {
+        assertSimpleTest(() -> {
+            run("26", "타파스-1,제로콜라-1");
+            assertThat(output()).contains("<혜택 내역>" + LINE_SEPARATOR + "없음");
+        });
+    }
 
 //    @Test
 //    void 날짜_예외_테스트() {
