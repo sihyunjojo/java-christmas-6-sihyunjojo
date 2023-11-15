3bcde06bc4bb3f7c5374417e5a6c1ed15900d055
sihyunjojo <si4018@naver.com>
2023-11-15 16:59:57 +0900

    test: Util 클래스에 대한 테스트 구현

---
+++ src/test/java/christmas/util/UtilTest.java
@@ -0,0 +1,28 @@
+package christmas.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.global.FoodMenu;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class UtilTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"티본스테이크", "바비큐립", "초코케이크"})
+    @DisplayName("findFoodMenuByName에 존재하는 메뉴를 넣으면, 그 메뉴를 반환해준다.")
+    void testFindFoodMenuByName(String menuName) {
+        FoodMenu foundMenu = Util.findFoodMenuByName(menuName);
+
+        assertEquals(foundMenu.getName(),menuName);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"존재하지않는메뉴", "라면", "피자"})
+    @DisplayName("findFoodMenuByName에 존재하지 않는 메뉴를 넣으면, null을 반환해준다.")
+    void testFindFoodMenuByName_invalidMenu(String menuName) {
+        FoodMenu foundMenu = Util.findFoodMenuByName(menuName);
+
+        assertNull(foundMenu);
+    }
+}
