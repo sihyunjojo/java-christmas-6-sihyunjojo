0809b0d59bc608d1a203b787f936c8165579214c
sihyunjojo <si4018@naver.com>
2023-11-15 16:26:39 +0900

    feat: 상수 추가

--- src/main/java/christmas/global/constants/DiscountDateConstants.java
+++ src/main/java/christmas/global/constants/DiscountDateConstants.java
@@ -5,9 +5,9 @@ import java.time.LocalDate;
 public class DiscountDateConstants {
     public static final int DISCOUNT_EVENT_YEAR = 2023;
     public static final int DISCOUNT_EVENT_MONTH = 12;
-    public static final LocalDate D_DAY_DISCOUNT_START_DAY = LocalDate.of(2023, 12, 1);
-    public static final LocalDate D_DAY_DISCOUNT_END_DAY = LocalDate.of(2023, 12, 25);
-    public static final LocalDate DECEMBER_DISCOUNT_START_DAY = LocalDate.of(2023, 12, 1);
-    public static final LocalDate DECEMBER_DISCOUNT_END_DAY = LocalDate.of(2023, 12, 31);
+    public static final LocalDate D_DAY_DISCOUNT_START_DAY = LocalDate.of(DISCOUNT_EVENT_YEAR, DISCOUNT_EVENT_MONTH, 1);
+    public static final LocalDate D_DAY_DISCOUNT_END_DAY = LocalDate.of(DISCOUNT_EVENT_YEAR, DISCOUNT_EVENT_MONTH, 25);
+    public static final LocalDate DECEMBER_DISCOUNT_START_DAY = LocalDate.of(DISCOUNT_EVENT_YEAR, DISCOUNT_EVENT_MONTH, 1);
+    public static final LocalDate DECEMBER_DISCOUNT_END_DAY = LocalDate.of(DISCOUNT_EVENT_YEAR, DISCOUNT_EVENT_MONTH, 31);
 
 }
