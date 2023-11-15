fbfc4e549fc67bfe9a29a0cab6b192dd036925ee
sihyunjojo <si4018@naver.com>
2023-11-15 18:23:49 +0900

    test: DisplayName 추가

--- src/test/java/christmas/global/EventBadgeTest.java
+++ src/test/java/christmas/global/EventBadgeTest.java
@@ -11,25 +11,25 @@ class EventBadgeTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1000, 2000, 3000})
-    @DisplayName("")
+    @DisplayName("총혜택 금액이 5000이하면, EventBadge.no를 반환해준다.")
     void testDetermineEventBadgeCaseNo(int price) {
         Assertions.assertEquals(EventBadge.no, EventBadge.determineEventBadge(price));
     }
     @ParameterizedTest
     @ValueSource(ints = {5000, 5232, 9999})
-    @DisplayName("")
+    @DisplayName("총혜택 금액이 5000이상 10000미만 이면, EventBadge.star를 반환해준다.")
     void testDetermineEventBadgeCaseStar(int price) {
         Assertions.assertEquals(EventBadge.star, EventBadge.determineEventBadge(price));
     }
     @ParameterizedTest
     @ValueSource(ints = {10000, 15000, 19999})
-    @DisplayName("")
+    @DisplayName("총혜택 금액이 10000이상 20000미만 이면, EventBadge.tree를 반환해준다.")
     void testDetermineEventBadgeCaseTree(int price) {
         Assertions.assertEquals(EventBadge.tree, EventBadge.determineEventBadge(price));
     }
     @ParameterizedTest
     @ValueSource(ints = {20000, 99999, 100000})
-    @DisplayName("")
+    @DisplayName("총혜택 금액이 20000이상 이면, EventBadge.santa를 반환해준다.")
     void testDetermineEventBadgeCaseSanta(int price) {
         Assertions.assertEquals(EventBadge.santa, EventBadge.determineEventBadge(price));
     }
